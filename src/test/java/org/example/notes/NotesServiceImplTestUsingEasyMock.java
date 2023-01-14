a1feb468b6e00128bc1a77efe9c9a46d5d72d82a
BigMurloc <s21159@pjwstk.edu.pl>
2023-01-14 16:27:56 +0100

    z3

--- src/test/java/org/example/notes/NotesServiceImplTestUsingEasyMock.java
+++ src/test/java/org/example/notes/NotesServiceImplTestUsingEasyMock.java
@@ -2,22 +2,33 @@ package org.example.notes;
 
 import org.easymock.EasyMock;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.easymock.EasyMock.expect;
+import static org.easymock.EasyMock.replay;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.mock;
 
 class NotesServiceImplTestUsingEasyMock {
 
     private final NotesStorage mock = EasyMock.createMock(NotesStorage.class);
+
     @Test
     void shouldAddNoteSuccessfully() {
         //given
         String name = "John";
         final NotesServiceImpl sut = NotesServiceImpl.createWith(mock);
         final Note note = new Note(name, 2);
-
-        //when
-        sut.add(note);
+        expect(mock.getAllNotesOf(name)).andReturn(List.of(note));
+        replay(mock);
 
         //then
         assertThat(sut.averageOf(name)).isEqualTo(2);
@@ -28,7 +39,7 @@ class NotesServiceImplTestUsingEasyMock {
         //given
         String student = "John";
         String otherStudent = "Alice";
-        final NotesServiceImpl sut = NotesServiceImpl.createWith(new NoteStorageMock());
+        final NotesServiceImpl sut = NotesServiceImpl.createWith(mock);
         final Note studentNote = new Note(student, 4);
         final Note studentNote1 = new Note(student, 6);
         final Note studentNote2 = new Note(student, 2);
@@ -36,13 +47,9 @@ class NotesServiceImplTestUsingEasyMock {
         final Note otherStudentNote1 = new Note(otherStudent, 4);
         final Note otherStudentNote2 = new Note(otherStudent, 2);
 
-        //when
-        sut.add(studentNote);
-        sut.add(studentNote1);
-        sut.add(studentNote2);
-        sut.add(otherStudentNote);
-        sut.add(otherStudentNote1);
-        sut.add(otherStudentNote2);
+        expect(mock.getAllNotesOf(student)).andReturn(List.of(studentNote, studentNote1, studentNote2));
+        expect(mock.getAllNotesOf(otherStudent)).andReturn(List.of(otherStudentNote, otherStudentNote1, otherStudentNote2));
+        replay(mock);
 
         //then
         assertThat(sut.averageOf(student)).isEqualTo(4);
@@ -53,15 +60,13 @@ class NotesServiceImplTestUsingEasyMock {
     void shouldCalculateAverageNote() {
         //given
         String name = "John";
-        final NotesServiceImpl sut = NotesServiceImpl.createWith(new NoteStorageMock());
+        final NotesServiceImpl sut = NotesServiceImpl.createWith(mock);
         final Note note1 = new Note(name, 2);
         final Note note2 = new Note(name, 3);
         final Note note3 = new Note(name, 4);
 
-        //when
-        sut.add(note1);
-        sut.add(note2);
-        sut.add(note3);
+        expect(mock.getAllNotesOf(name)).andReturn(List.of(note1, note2, note3));
+        replay(mock);
 
         //then
         assertThat(sut.averageOf(name)).isEqualTo(3);
@@ -77,4 +82,50 @@ class NotesServiceImplTestUsingEasyMock {
         assertDoesNotThrow(sut::clear);
     }
 
+
+    @Test
+    void shouldAddToList() {
+        NotesStorage mock = mock(NotesStorage.class);
+
+        List<Note> notes = new ArrayList<>();
+        Note note = new Note("John", 4);
+
+        doAnswer(new Answer<Void>() {
+            @Override
+            public Void answer(InvocationOnMock invocation) throws Throwable {
+                Note note = invocation.getArgument(0);
+                notes.add(note);
+
+                return null;
+            }
+        }).when(mock).add(note);
+
+        mock.add(note);
+
+        Mockito.verify(mock).add(note);
+        assertTrue(notes.contains(note));
+    }
+
+    @Test
+    void shouldClearList() {
+        NotesStorage mock = mock(NotesStorage.class);
+
+        List<Note> notes = new ArrayList<>();
+        Note note = new Note("John", 4);
+        notes.add(note);
+
+        doAnswer(new Answer<Void>() {
+            @Override
+            public Void answer(InvocationOnMock invocation) throws Throwable {
+                notes.clear();
+
+                return null;
+            }
+        }).when(mock).clear();
+
+        mock.clear();
+
+        Mockito.verify(mock).clear();
+        assertTrue(notes.isEmpty());
+    }
 }
